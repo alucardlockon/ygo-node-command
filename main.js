de90ecb8c5416bd3deba332acbe84d8408154471
alucardlockon <alucardlockon3@gmail.com>
2017-09-20 15:10:33 +0800

    adjust commands

--- main.js
+++ main.js
@@ -9,24 +9,44 @@ const rl = readline.createInterface({
 
 console.log('welcome to the ygo vrains...');
 
-const commands = [{
+const commands = [
+  {
     commandName: 'draw',
     func: draw
   },
   {
     commandName: 'readdeck',
     func: readDeck
+  },
+  {
+    commandName: 'draw-random',
+    func: drawRandom
+  },
+  {
+    commandName: 'shuffle',
+    func: shuffle
+  },
+  {
+    commandName: 'hand',
+    func: hand
   }
 ]
 let deck_p1;
 let deck_p1_rt;
+let hand_p1;
+let grav_p1;
 let deck_p2;
 let deck_p2_rt;
+let hand_p2;
+let grav_p2;
+
 const preStr='> ';
 
-readDeck('ABC')
-draw()
-repl();
+readDeck('EMmss')
+shuffle()
+draw(5)
+hand()
+repl()
 
 function repl() {
   rl.question('input your command:\n', (command) => {
@@ -64,13 +84,35 @@ function readDeck(deckName){
   //   }
     deck_p1=JSON.parse(fs.readFileSync(dir,'utf8'));
     deck_p1_rt=deck_p1;
+    hand_p1=[];
+    grav_p1=[];
   // });
 }
 
-function draw(count) {
+function shuffle() {
+  deck_p1_rt.main=_.shuffle(deck_p1_rt.main);
+}
+
+function hand() {
+  console.log(preStr+'your hand cards : '+hand_p1.join(','));
+}
+
+function drawRandom(count) {
   if (!count) count = 5;
-  console.log(preStr+'draw' + count + 'cards');
+  console.log(preStr+'draw ' + count + ' cards');
   console.log(preStr+'result:'+_.map(_.times(count,x=>_.random(deck_p1_rt.main.length-1)),x=>deck_p1_rt.main[x]).join(','));
 }
 
+function draw(count) {
+  if (!count) count = 1;
+  console.log(preStr+'draw ' + count + ' cards');
+  console.log(preStr+'result:'+_.times(count,drawOne).join(','));
+}
+
+function drawOne() {
+  const card=_.head(deck_p1_rt.main);
+  deck_p1_rt.main.shift();
+  hand_p1.push(card);
+  return card;
+}
 
